57e4438b18af4fd722c1772e40ae2159ce3ac658
IrisMaenhout <irismaen@student.arteveldehs.be>
2024-03-13 23:30:36 +0100

    updated contact card

--- portfolio-app/src/components/home/contact/Contact.jsx
+++ portfolio-app/src/components/home/contact/Contact.jsx
@@ -4,21 +4,8 @@ import styles from './contact.module.css';
 
 function Contact(props) {
     return (
-        <div>
-            <h2 className='title'>Contact</h2>
-            
-            <div className={styles.flexContainer}>
-                <div className={styles.contactCard}>
-                    <ContactCard />
-                </div>
-                
-                <div className={styles.imgContainer}>
-                    <img src="https://parade.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTkzNDMwNzY3MzQxMTUxODEx/why-are-my-text-messages-green.jpg" alt="" />
-
-                </div>
-            </div>
-            
-
+        <div className={styles.marginContainer}>
+            <ContactCard />
         </div>
     );
 }
--- portfolio-app/src/components/home/contact/contactCard/ContactCard.jsx
+++ portfolio-app/src/components/home/contact/contactCard/ContactCard.jsx
@@ -8,32 +8,44 @@ function ContactCard(props) {
     return (
         
         <div className={styles.card}>
-            <div className={styles.socialMedia}>
-                <Link to={''}>
-                    <i className="fa-brands fa-linkedin"></i>
-                </Link>
 
-                <Link to={''}>
-                    <i className="fa-brands fa-github"></i>
-                </Link>
+            <div className={styles.content}>
+                <div className={styles.contactInfo}>
+                    <h5>Contact</h5>
+                    <h2 className='gradientText'>Zin om samen te werken?</h2>
+                    <p>Contacteer mij gerust en laten we er samen in vliegen.</p>
 
-                <Link to={''}>
-                    <i className="fa-brands fa-facebook-messenger"></i>
-                </Link>
-                
-                
+                    <div className={styles.phoneNrEmailDiv}>
+                        <i className="fa-solid fa-envelope"></i>
+                        <a href="mailto:irismaenhout@gmail.com">irismaenhout@gmail.com</a>
+                    </div>
+
+                    <div className={styles.phoneNrEmailDiv}>
+                        <i className="fa-solid fa-phone"></i>
+                        <a href="tel:+32 4325 2909">+32 4325 2909</a>
+                    </div>
+
+                </div>
+
+
+                <div className={styles.socialMedia}>
+                    <Link to={''}>
+                        <i className="fa-brands fa-linkedin"></i>
+                    </Link>
+
+                    <Link to={''}>
+                        <i className="fa-brands fa-github"></i>
+                    </Link>
+
+                    <Link to={''}>
+                        <i className="fa-brands fa-facebook-messenger"></i>
+                    </Link>
+                    
+                    
+                </div>
             </div>
 
-            <div className={styles.contactInfo}>
-                <div>
-                    <h4>Email</h4>
-                    <a href="mailto:irismaenhout@gmail.com">irismaenhout@gmail.com</a>
-                </div>
-
-                <div>
-                    <h4>Telefoon nummer</h4>
-                    <a href="tel:+32 4325 2909">+32 4325 2909</a>
-                </div>
+            <div className={styles.image}>
 
             </div>
                 
